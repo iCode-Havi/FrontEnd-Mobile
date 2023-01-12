0784a892f77b7ce430af0138d20041b135a11ad3
Diniki-Jayakody <jayakodyjadn.19@uom.lk>
2023-01-12 16:58:39 +0530

    Farmer  Transporter Forms

--- src/screens/auth/register/FarmerRegistration.js
+++ src/screens/auth/register/FarmerRegistration.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { ScrollView, Text, TextInput, View, TouchableHighlight, SafeAreaView } from 'react-native';
 
@@ -8,6 +8,25 @@ import COMMON_STYLES from '../../../common/styles/styles';
 import COLORS from '../../../common/colors/colors';
 
 export default function FarmerRegistration({ navigation }) {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [nic, setNic] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact1, setContact1] = useState("");
+  const [contact2, setContact2] = useState("");
+  const [location, setLocation] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [cardNo, setCardNo] = useState("");
+  const [bank, setBank] = useState("");
+  const [holdersName, setHoldersName] = useState("");
+  const [expDate, setExpDate] = useState("");
+  const [expMonth, setExpMonth] = useState("");
+  const [cvv, setCVV] = useState("");
+
 
   const icon_arrow_circle_left = <FontAwesome5 name={'arrow-circle-left'} color={COLORS.primary} size={30} onPress={() => navigation.navigate('AssistantRegistration')} />;
   const icon_arrow_circle_right = <FontAwesome5 name={'arrow-circle-right'} color={COLORS.primary} size={30} onPress={() => navigation.navigate('TransporterRegistration')} />;
@@ -25,16 +44,20 @@ export default function FarmerRegistration({ navigation }) {
             </TouchableHighlight>
         </View>
         <View style={COMMON_STYLES.registrationForm}>
-          <TextInput style={COMMON_STYLES.inputFirst} placeholder={STRINGS.firstName}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.lastName}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.nic}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.insertImage}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.email}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.password}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.confirmPassword}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact1}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact2}></TextInput>
-          <TextInput style={COMMON_STYLES.inputLast} placeholder={STRINGS.location}></TextInput>
+          <TextInput style={COMMON_STYLES.inputFirst} placeholder={STRINGS.firstName} onChange={(val)=> setFirstName(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.lastName} onChange={(val)=> setLastName(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.nic} onChange={(val)=> setNic(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.insertImage} onChange={(val)=> setImgUrl(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.email} onChange={(val)=> setEmail(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.password} onChange={(val)=> setPassword(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.confirmPassword} onChange={(val)=> setConfirmPassword(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact1} onChange={(val)=> setContact1(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact2} onChange={(val)=> setContact2(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.inputLast} placeholder={STRINGS.location} onChange={(val)=> setLocation(val)}></TextInput>
+
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.password} onChange={(val)=> setPassword(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.inputLast} placeholder={STRINGS.confirmPassword} onChange={(val)=> setConfirmPassword(val)}></TextInput>
+
           <View style={COMMON_STYLES.buttonContainer}>
             <TouchableHighlight style={COMMON_STYLES.button}>
               {icon_arrow_circle_left}
--- src/screens/auth/register/TransporterRegistration.js
+++ src/screens/auth/register/TransporterRegistration.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { ScrollView, Text, TextInput, View, TouchableHighlight, SafeAreaView } from 'react-native';
 
 //COMMON
@@ -6,6 +6,17 @@ import STRINGS from '../../../common/strings/strings';
 import COMMON_STYLES from '../../../common/styles/styles';
 
 export default function TransporterRegistration({ navigation }) {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [nic, setNic] = useState("");
+  const [contact1, setContact1] = useState("");
+  const [contact2, setContact2] = useState("");
+  const [location, setLocation] = useState("");
+  const [transportMode, setTransportMode] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
 
 
   return (
@@ -22,16 +33,16 @@ export default function TransporterRegistration({ navigation }) {
             </TouchableHighlight>
         </View>
         <View style={COMMON_STYLES.registrationForm}>
-          <TextInput style={COMMON_STYLES.inputFirst} placeholder={STRINGS.firstName}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.lastName}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.nic}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.email}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact1}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact2}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.location}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.addImages}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.password}></TextInput>
-          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.confirmPassword}></TextInput>
+          <TextInput style={COMMON_STYLES.inputFirst} placeholder={STRINGS.firstName}  onChange={(val)=> setFirstName(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.lastName} onChange={(val)=> setLastName(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.nic} onChange={(val)=> setNic(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact1} onChange={(val)=> setContact1(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.contact2} onChange={(val)=> setContact2(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.location} onChange={(val)=> setLocation(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder="Transport Mode" onChange={(val)=> setTransportMode(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.addImages} onChange={(val)=> setImgUrl(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.password} onChange={(val)=> setPassword(val)}></TextInput>
+          <TextInput style={COMMON_STYLES.input} placeholder={STRINGS.confirmPassword} onChange={(val)=> setConfirmPassword(val)}></TextInput>
        
         </View>
       </ScrollView>
